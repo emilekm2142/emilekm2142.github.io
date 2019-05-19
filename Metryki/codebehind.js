d4f0f35d7c4a373e80f3af5636251480b2655c41
emile <emilm7843@gmail.com>
2019-05-19 09:10:15 +0200

    dodanie plane

--- Metryki/codebehind.js
+++ Metryki/codebehind.js
@@ -27,6 +27,13 @@ function langChange(name){
        d[0].innerHTML = langData[name][key];
     }
 }
+function drawEntirePlaneUsingMetricsInTextarea(){
+    metrics = new Function("p1","p2",document.getElementById("codeInput").value);
+    p1={x:-2,y:-3};
+    p2={x:-4,y:4};
+    console.log(metrics(p1,p2));
+    drawPlane(metrics);
+}
 function drawSphereUsingMetricsInTextarea(){
     //getting the function
     metrics = new Function("p1","p2",document.getElementById("codeInput").value);
@@ -34,6 +41,48 @@ function drawSphereUsingMetricsInTextarea(){
     p2={x:-4,y:4};
     console.log(metrics(p1,p2));
     drawSphere(metrics);
+}
+function drawPlane(metrics){
+    c.fillStyle="white"
+    c.fillRect(0,0,width,height);
+     var xc=0,yc=0;
+     xc = Number(document.getElementById("x").value);
+     yc =Number(document.getElementById("y").value);
+    
+     var origin ={x:(width/2)+xc,y:(height/2)+yc};
+    console.log(origin);
+    //finding max distance from origin
+    let max = 0;
+    for (var row =0; row<height; row++){
+        for (var column =0; column<width; column++){
+            var currentPoint = {x:column,y:row};
+            
+           //console.log(JSON.stringify(currentPoint));
+            var distanceFromOrigin = metrics(currentPoint,origin);
+            if (distanceFromOrigin>max) max=distanceFromOrigin;
+        }
+    }
+
+    //drawing
+     for (var row =0; row<height; row++){
+         for (var column =0; column<width; column++){
+             var currentPoint = {x:column,y:row};
+             
+            //console.log(JSON.stringify(currentPoint));
+             var distanceFromOrigin = metrics(currentPoint,origin);
+             
+             
+                 //point is in a sphere
+                 var newCoordinates=currentPoint;
+                 ///var newCoordinates = translateCartesianToCanvas(currentPoint);
+
+                 c.fillStyle = `rgba(0,0,0,${distanceFromOrigin/max})`;
+                 c.fillRect(newCoordinates.x,newCoordinates.y,1,1);
+             
+             
+         } 
+     }
+     drawAxes();
 }
 function drawSphere(metrics){
    c.fillStyle="white"
@@ -82,4 +131,5 @@ function drawAxes(){
 function translateCartesianToCanvas(point){
     return point;
 }
-document.getElementById("submit").onclick = drawSphereUsingMetricsInTextarea;
+document.getElementById("submit-sphere").onclick = drawSphereUsingMetricsInTextarea;
+document.getElementById("submit-plane").onclick = drawEntirePlaneUsingMetricsInTextarea;
